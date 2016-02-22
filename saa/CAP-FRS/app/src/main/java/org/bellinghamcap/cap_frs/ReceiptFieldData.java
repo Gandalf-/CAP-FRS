1580440a2729e650408d6e3f580f3ad819423398
thesethwalker <thesethwalker@users.noreply.github.com>
2016-02-22 02:49:39 -0800

    Beta release version

---
+++ saa/CAP-FRS/app/src/main/java/org/bellinghamcap/cap_frs/ReceiptFieldData.java
@@ -0,0 +1,19 @@
+package org.bellinghamcap.cap_frs;
+
+public class ReceiptFieldData {
+    public String editName;
+    public String editCAPID;
+    public String editAircraftTail;
+    public String editAircraftModel;
+    public String editMissionDate;
+    public String editMissionSymbol;
+    public String editSorties;
+    public String editHobbsTime;
+    public String editTotalGallons;
+    public String editFuelCost;
+    public String editRemarks;
+    public String editVendorName;
+    public String editVendorLocation;
+    public String editServiceDate;
+    public double avgFuelBurn;
+}
